bc35f8db8b2dc78a1f56a449e1d1b5b9a35d6bc8
Davy Jacquemard <davy.jacquemard@edu.univ-fcomte.fr>
2021-01-02 12:32:45 +0100

    Footer done

--- wp-content/themes/huawei/footer.php
+++ wp-content/themes/huawei/footer.php
@@ -3,30 +3,30 @@
         <div class="row">
             <div class="footer-top">
                 <div class="footer-top__contact">
-                    <h2 class="h2">Besoin d'aide ?</h2>
+                    <h2 class="h2 footer-top__contact__title">Besoin d'aide ?</h2>
                     <p class="p">Nous sommes à votre écoute ! <a href="mailto:watch@huawei.com"><span class="link">Contactez-nous</span></a></p>
-                    <img class="logo-footer" src="static/img/logo/logo-v1.svg" alt="logo Huawei">
+                    <img class="logo-footer footer-top__contact__logo" src="<?php echo get_template_directory_uri() . '/static/img/logo/logo-v1.svg' ?>" alt="logo Huawei">
                 </div>
-                <div class="footer-top__watch">
-                    <div class="headline">Nos montres</div>
+                <div class="footer-top__menu">
+                    <div class="headline footer-top__menu__title">Nos montres</div>
                     <ul>
                         <li><a href="#" class="p">Huawei Watch GT2</a></li>
                         <li><a href="#" class="p">Huawei Watch GT2 Pro</a></li>
                     </ul>
                 </div>
-                <div class="footer-top__blog">
-                    <div class="headline">Blog</div>
+                <div class="footer-top__menu">
+                    <div class="headline footer-top__menu__title">Blog</div>
                     <ul>
                         <li><a href="#" class="p">Nos actualités</a></li>
                     </ul>
                 </div>
                 <div class="footer-top__social">
-                    <div class="headline">Rejoignez-nous</div>
-                    <h3 class="h4">Suivez-nous sur nos réseaux sociaux</h3>
+                    <div class="headline footer-top__social__headline">Rejoignez-nous</div>
+                    <h3 class="h4 footer-top__social__title">Suivez-nous sur nos réseaux sociaux</h3>
                     <ul class="footer-top__social__list">
-                        <li><a href="#" class="icon-social-facebook footer-icon-socialmedia"></a></li>
-                        <li><a href="#" class="icon-social-twitter footer-icon-socialmedia"></a></li>
-                        <li><a href="#" class="icon-social-instagram footer-icon-socialmedia"></a></li>
+                        <li><a href="https://www.facebook.com/HuaweiEuropean" rel="nofollow" target="_blank" class="icon-social-facebook footer-icon"></a></li>
+                        <li><a href="https://twitter.com/HuaweiFr" rel="nofollow" target="_blank" class="icon-social-twitter footer-icon"></a></li>
+                        <li><a href="https://www.instagram.com/huawei/" rel="nofollow" target="_blank" class="icon-social-instagram footer-icon"></a></li>
                     </ul>
                 </div>
             </div>
