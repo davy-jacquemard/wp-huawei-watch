1fdf160adc19f55a9b6647cc75f29359dbc74a5b
Davy Jacquemard <davy.jacquemard@edu.univ-fcomte.fr>
2021-01-20 14:25:17 +0100

    #FO - fix video autoplay + section finition done

--- wp-content/themes/huawei/build/scripts/global.js
+++ wp-content/themes/huawei/build/scripts/global.js
@@ -12,27 +12,30 @@ const items = document.querySelectorAll('[js-home-vertical-slider-item]');
 const dots = document.querySelectorAll('[js-home-vertical-slider-dot]');
 const texts = document.querySelectorAll('[js-home-vertical-slider-text]');
 const activeClass = 'is-active';
-window.addEventListener('scroll', () => {
-  items.forEach((item, index) => {
-    let itemPosTop = item.getBoundingClientRect().top;
-    let itemHeight = item.offsetHeight;
 
-    if (itemPosTop - window.innerHeight / 2 < 0 && itemPosTop - window.innerHeight / 2 > -itemHeight) {
-      dots.forEach(dot => {
-        if (dot.classList.contains(activeClass)) {
-          dot.classList.remove(activeClass);
-        }
-      });
-      dots[index].classList.add(activeClass);
-      texts.forEach(text => {
-        if (text.classList.contains(activeClass)) {
-          text.classList.remove(activeClass);
-        }
-      });
-      texts[index].classList.add(activeClass);
-    }
+if (slider) {
+  window.addEventListener('scroll', () => {
+    items.forEach((item, index) => {
+      let itemPosTop = item.getBoundingClientRect().top;
+      let itemHeight = item.offsetHeight;
+
+      if (itemPosTop - window.innerHeight / 2 < 0 && itemPosTop - window.innerHeight / 2 > -itemHeight) {
+        dots.forEach(dot => {
+          if (dot.classList.contains(activeClass)) {
+            dot.classList.remove(activeClass);
+          }
+        });
+        dots[index].classList.add(activeClass);
+        texts.forEach(text => {
+          if (text.classList.contains(activeClass)) {
+            text.classList.remove(activeClass);
+          }
+        });
+        texts[index].classList.add(activeClass);
+      }
+    });
   });
-});
+}
 
 /***/ }),
 
