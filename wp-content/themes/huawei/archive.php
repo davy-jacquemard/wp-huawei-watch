e28eaea49886b1de058cb1445628f7d3202cdc16
hugzeffner <schuhler.hugo57@gmail.com>
2021-01-21 11:34:48 +0100

    #FO archive container

--- wp-content/themes/huawei/archive.php
+++ wp-content/themes/huawei/archive.php
@@ -5,7 +5,23 @@ Template Name: Archives articles
 ?>
 
 <?php get_header(); ?>
+<div class="main">
 
-<h1 class="h1">Page archive</h1>
 
+<?php
+/*
+Template Name: Page d'archives
+*/
+?>
+<div class="main">
+    <h1 class="h1">Page archive</h1>
+    <div id="content" role="main">
+
+        <?php the_post(); ?>
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+
+    </div><!-- #content -->
+
+    
+</div>
 <?php get_footer() ?>
