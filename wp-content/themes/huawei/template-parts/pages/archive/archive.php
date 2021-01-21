a1268e261d565ce596dca53764d722f9c8598641
Davy Jacquemard <davy.jacquemard@edu.univ-fcomte.fr>
2021-01-21 19:32:05 +0100

    #FO - fix cards

--- wp-content/themes/huawei/template-parts/pages/archive/archive.php
+++ wp-content/themes/huawei/template-parts/pages/archive/archive.php
@@ -1,4 +1,4 @@
 <div class="main">
     <!--post-->
-    <?php get_template_part('template-parts/pages/archive/includes/post'); ?>
+    <?php get_template_part('template-parts/pages/archive/includes/archive-posts'); ?>
 </div>
---
+++ wp-content/themes/huawei/template-parts/pages/archive/includes/archive-posts.php
@@ -0,0 +1,32 @@
+<?php
+$articleHeader = get_field('article_header');
+$id = get_the_ID();
+$date = get_the_date( 'd F Y', $id);
+$taxo = get_the_terms($id, 'article_category');
+$card = array(
+    'link'      => get_permalink(),
+    'image_url' => $articleHeader['image']['sizes']['medium_large'],
+    'image_alt' => $articleHeader['image']['alt'],
+    'category'  => $taxo[0]->name,
+    'title'     => $articleHeader['title'],
+    'date'      => $date,
+)
+ ?>
+<div>
+<!--     <pre>
+     <?php print_r ($card); ?> </br>
+     </pre> -->
+
+    
+     
+
+  <!--    <?php print_r(get_the_category($id)); ?> -->
+
+</div>
+<div class="row">
+    <div class="news-container">
+        <?php 
+        set_query_var( 'card', $card );
+        get_template_part( 'template-parts/components/cards/card', 'news'); ?>
+    </div>
+</div>
