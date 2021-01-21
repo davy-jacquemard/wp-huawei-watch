9f38b1452ae522a009222126510a5e5fe6936fd3
Davy Jacquemard <davy.jacquemard@edu.univ-fcomte.fr>
2021-01-21 18:05:20 +0100

    #FO - last news in progress

--- wp-content/themes/huawei/template-parts/components/shared-block/last-news.php
+++ wp-content/themes/huawei/template-parts/components/shared-block/last-news.php
@@ -1,35 +1,62 @@
-
-yo
 <?php
 
-    // The Query
-    $args = array(
-                    'post_type' => 'articles',
-                    'post_status' => 'publish',
-                    'posts_per_page'=>4,
-                    'order'=>'ASC',
-                    'orderby'=>'date',
-                    );
+$lastNews = get_field('last_news', 'option');
+$surtitle = $lastNews['box_title']['sur_title'];
+$title = $lastNews['box_title']['title'];
+$link = $lastNews['link'];
 
-    $the_query = new WP_Query( $args );
+/* echo '<pre>';
+print_r($surtitle);
+echo '</pre>';
+ */
 
-    // The Loop
-    if ( $the_query->have_posts() ) {
- 
-        while ( $the_query->have_posts() ) {
-            $the_query->the_post();
-            //your html here for latest 2
+$args = array(
+    'post_type' => 'articles',
+    'post_status' => 'publish',
+    'posts_per_page' => 4,
+    'order' => 'ASC',
+    'orderby' => 'date',
+);
 
+$the_query = new WP_Query($args);
 ?>
-COUcou
-<?php 
-    }
 
-    } else {
-        // no posts found
-    }
-    /* Restore original Post Data */
-    wp_reset_postdata();
-?>
-           
-    
+<section class="w-flex last-news">
+    <div class="container">
+        <div class="last-news__header">
+            <div class="w-title">
+                <div class="headline"><?php echo $surtitle ?></div>
+                <h2 class="h2"><?php echo $title ?></h2>
+            </div>
+            <div class="btn-container">
+                <a href="<?php echo $link['url'] ?>" class="btn btn--secondary"><?php echo $link['title'] ?></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="last-news__list">
+
+
+                <?php
+                if ($the_query->have_posts()) {
+                    while ($the_query->have_posts()) {
+                        $the_query->the_post();
+
+                        $fields = get_field('article_header');
+
+                        $title = $fields['title']; 
+                        $image = $fields['image'];?>
+
+                        <?php echo $title ?>
+                        <?php echo $image['url'] ?>
+
+                <?php
+                    }
+                }
+                wp_reset_postdata();
+                ?>
+
+
+            </div>
+        </div>
+    </div>
+</section>
