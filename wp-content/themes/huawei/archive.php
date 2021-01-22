22128d4e88d43d85eb4fc8c22789dda3ea7db8fc
hugzeffner <schuhler.hugo57@gmail.com>
2021-01-22 17:59:31 +0100

    #FO - archive -fix

--- wp-content/themes/huawei/archive.php
+++ wp-content/themes/huawei/archive.php
@@ -6,25 +6,6 @@ Template Name: Archives articles
 
 <?php get_header(); ?>
     <div class="main">
-    <?php if ( have_posts() ) : ?>
-        <h1 class="h1">Retrouvez tous les articles</h1>
-        <div class="row archive-container">
-            <?php
-            /* Start the Loop */
-            while ( have_posts() ) :
-                the_post();
-                get_template_part( 'template-parts/pages/archive/archive' );
-                
-            endwhile;
-
-            the_posts_navigation();
-
-            else :
-
-            get_template_part( 'template-parts/content', 'none' );
-
-            endif;
-            ?>            
-        </div>  
+        <?php get_template_part('template-parts/pages/archive/archive')?>         
     </div>
 <?php get_footer() ?>
