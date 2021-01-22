c0e00761451f7af4712351c8d56e1d2fdc60f902
Déborah Gluck <deborah.gluck@icloud.com>
2021-01-22 15:54:50 +0100

    #FO gamme-app.php

--- wp-content/themes/huawei/template-parts/pages/gamme/includes/gamme-app.php
+++ wp-content/themes/huawei/template-parts/pages/gamme/includes/gamme-app.php
@@ -1,9 +1,10 @@
 <?php
 $app = get_field('gamme_app');
 $title_box = $app['title_box'];
-$pictures = $app['pictures'];
-$title_box_inner = $app['title_box_inner'];
-$links = $app['links'];
+$slides = $app['slides'];
+$title = $slides['title'];
+$text = $slides['text'];
+$picture = $slides['picture'];
 
 ?>
 
@@ -12,19 +13,36 @@ $links = $app['links'];
         <div class="w-title">
             <div class="headline"><?php echo $title_box['surtitle'] ?></div>
             <h2 class="h2"><?php echo $title_box['title'] ?></h2>
+            <h3 class="h3"><?php echo $app['title_inner'] ?></h3>
         </div>
-        <img src="<?php echo $pictures['picture']['url'] ?>" alt="<?php echo $pictures['picture']['alt'] ?>">
-    </div>
-    <div class="container">
-        <h3 class="h3"><?php echo $title_box_inner['title-inner-first'] ?></h3>
-        <h4 class="h4"><?php echo $title_box_inner['title-inner-second'] ?></h4>
-        <ul class="primary-list">
-            <li class="li"><a class="link"><?php echo $links['link'][1] ?></a></li>
-            <li class="li"><a class="link"><?php echo $links['link'][2] ?></a></li>
-            <li class="li"><a class="link"><?php echo $links['link'][3] ?></a></li>
-            <li class="li"><a class="link"><?php echo $links['link'][4] ?></a></li>
-            <li class="li"><a class="link"><?php echo $links['link'][5] ?></a></li>
-            <li class="li"><a class="link"><?php echo $links['link'][6] ?></a></li>
-        </ul>
+        <div class="row">
+            <?php
+            // Check rows exists.
+            if( have_rows('pictures') ):
+
+            // Loop through rows.
+            while( have_rows('pictures') ) : the_row();
+
+            // Load sub field value.
+            $sub_value = get_sub_field('picture');
+            // Do something...
+                ?>
+            <img srcset="" sizes="" src="<?php echo esc_url($picture['url']); ?>"
+                alt="<?php echo esc_attr($picture['alt']); ?>">
+            <?php
+            // End loop.
+            endwhile;
+
+            // No value.
+            else :
+            // Do something...
+            endif;
+            ?>
+
+        </div>
+        <div class="row">
+            <h4 class="h4"><?php echo $title ?></h4>
+            <p class="p"><?php echo $text ?></p>
+        </div>
     </div>
 </section>
