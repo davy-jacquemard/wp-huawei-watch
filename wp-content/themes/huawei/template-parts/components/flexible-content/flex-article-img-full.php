57c44673bd6eb2c78dd1b5a52f6026687498d2ac
Florent <florent.chatelet@edu.univ-fcomte.fr>
2021-01-22 15:43:13 +0100

    #FO push flexible content

--- wp-content/themes/huawei/template-parts/components/flexible-content/flex-article-img-full.php
+++ wp-content/themes/huawei/template-parts/components/flexible-content/flex-article-img-full.php
@@ -5,8 +5,13 @@ $img_full = get_sub_field('image')
 ?> 
 
 
-<section class="w-flex flexible-img-full">
-    <div class="container-fluid">
-            <img class="flexible-img-full__img" src="<?php echo $img_full['url'] ?>" alt="">
+<section class="w-flex ">
+    <div class="flexible-img-full">
+
+            <div class="flexible-img-full__img-container container-fluid ">
+                <img class="flexible-img-full__img" src="<?php echo $img_full['url'] ?>" alt="">
+            </div>
+
     </div>
+
 </section>
