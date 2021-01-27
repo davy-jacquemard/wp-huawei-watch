c22286a2fed09faf677a69264e30fa83e3fdcb19
hugzeffner <schuhler.hugo57@gmail.com>
2021-01-27 21:42:16 +0100

    #FO - ACF variables/affichage

--- wp-content/themes/huawei/template-parts/pages/archive/includes/archive-header.php
+++ wp-content/themes/huawei/template-parts/pages/archive/includes/archive-header.php
@@ -8,8 +8,23 @@
 
 $page_for_posts_ID = get_option( 'page_for_posts' );
 $header = get_field('archive_header', $page_for_posts_ID);
+$titre = $header['title'];
+$intro = $header['text'];
+$link = $header['link'];
+$image = $header['image']['url'];
+
+
 
 echo '<pre>';
   print_r($header);
 echo '</pre>';
+
+
+
 ?>
+
+<p> <?php echo $titre; ?></p>
+
+<p> <?php echo $intro; ?></p>
+<a href="<?php $link; ?>">Bouton</a>
+<img src="<?php echo esc_url($image); ?>" alt="">
